ae062ac28bfb2b2f549488999b87712cc42b74ee
Luis Márquez <luisfmarquezsosa@gmail.com>
2025-04-24 21:09:53 -0400

    Redis Update

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ if (cluster.isMaster) {
 
         if (data.type === "registro") {
           const nombre = data.nombre;
-          const yaExiste = await redis.hexists("jugadores", nombre);
+          const yaExiste = await redis.hExists("jugadores", nombre);
           console.log("📥 Recibido registro:", nombre);
           if (yaExiste) {
             ws.send(JSON.stringify({ type: "error", mensaje: "Nombre ya en uso" }));
